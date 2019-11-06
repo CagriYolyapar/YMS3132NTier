3f07ed1e37d262e8752fd1ac8acfc601ab87c549
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2019-11-06 13:17:38 +0300

    Veritabanı sınıfları MODEL katmanı icin olusturuldu

---
+++ Project.MODEL/Entities/AppUserDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Entities
+{
+    public   class AppUserDetail:BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //Relational Properties
+
+        public virtual AppUser AppUser { get; set; }
+
+
+    }
+}
---
+++ Project.MODEL/Entities/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MODEL.Entities
+{
+    public class OrderDetail:BaseEntity
+    {
+        public int OrderID { get; set; }
+
+        public int ProductID { get; set; }
+
+        //Relational Properties
+
+        public virtual Product Product { get; set; }
+
+        public virtual Order Order { get; set; }
+
+
+    }
+}
